a6b5799b78d2943be3c0e0e2881d1b601f11c93d
Kolibrika666 <kolibrika666@gmail.com>
2023-12-09 17:06:41 +0700

    init: StartRadioPage

---
+++ src/StartPage/StartPageRadioApp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StartPageRadio.module.css";
+const StartPageRadioApp = () => {
+  return (
+    <div>
+      <h1>Заявки на радио</h1>
+      <h3>Отправляйте заявку и мы сыграем вашу любимую песню!</h3>
+    </div>
+  );
+};
+
+export default StartPageRadioApp;
