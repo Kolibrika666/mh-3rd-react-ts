18f8782e6e447498c992d9a4c9a60b0e90951ad8
Kolibrika666 <kolibrika666@gmail.com>
2023-12-09 17:06:27 +0700

    init: StartRadioPage

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import "./css/userData.css";
-import "./css/counter.css";
+
 import App from "./App";
 // import reportWebVitals from './reportWebVitals';
 
